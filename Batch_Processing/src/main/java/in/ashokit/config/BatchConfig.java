8231e1a44abc38547eb8ccc09f84e0106ccc11a5
Prathamesh Yadav <prathameshsyadav@gmail.com>
2024-10-02 11:40:59 +0530

    changing config files

--- Batch_Processing/src/main/java/in/ashokit/config/BatchConfig.java
+++ Batch_Processing/src/main/java/in/ashokit/config/BatchConfig.java
@@ -1,4 +1,4 @@
-package in.ashokit.config;
+/*package in.ashokit.config;
 
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
@@ -21,14 +21,17 @@ import org.springframework.core.task.TaskExecutor;
 import in.ashokit.entity.Customer;
 import in.ashokit.repo.CustomerRepo;
 
+
 @Configuration
 @EnableBatchProcessing
 public class BatchConfig {
 	
 	@Autowired
 	private CustomerRepo customerRepo;
+	
 	@Autowired
 	private StepBuilderFactory stepBuilderFactory;
+	
 	@Autowired
 	private JobBuilderFactory jobBuilderFactory;
 	
@@ -53,7 +56,7 @@ public class BatchConfig {
 		
 		lineTokenizer.setDelimiter(",");
 		lineTokenizer.setStrict(false);
-		lineTokenizer.setNames("id", "firstName", "lastName", "email", "gender", "contactNo", "country", "dob");
+		lineTokenizer.setNames("id","firstName","lastName","email","gender","contactNo","country","dob");
 		
 		BeanWrapperFieldSetMapper<Customer> fieldSetMapper = new BeanWrapperFieldSetMapper<Customer>();
 		
@@ -104,4 +107,115 @@ public class BatchConfig {
 	public Job job() {
 		return jobBuilderFactory.get("job").flow(step()).end().build();
 	}
+}
+*/
+
+
+
+package in.ashokit.config;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.item.data.RepositoryItemWriter;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.LineMapper;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import in.ashokit.entity.Customer;
+import in.ashokit.repo.CustomerRepo;
+
+@Configuration
+public class BatchConfig {
+
+    @Autowired
+    private CustomerRepo customerRepo;
+    
+    @Autowired
+    private JobRepository jobRepository;
+
+    @Autowired
+    private PlatformTransactionManager transactionManager;
+
+    // Source -- Item reader
+    @Bean
+    public FlatFileItemReader<Customer> customerReader() {
+        FlatFileItemReader<Customer> itemReader = new FlatFileItemReader<>();
+        itemReader.setResource(new FileSystemResource("src/main/resources/customers.csv"));
+        itemReader.setLinesToSkip(1);
+        itemReader.setName("csv-reader");
+        itemReader.setLineMapper(lineMapper());
+        return itemReader;
+    }
+
+    private LineMapper<Customer> lineMapper() {
+        DefaultLineMapper<Customer> lineMapper = new DefaultLineMapper<>();
+        DelimitedLineTokenizer lineTokenizer = new DelimitedLineTokenizer();
+        
+        lineTokenizer.setDelimiter(",");
+        lineTokenizer.setStrict(false);
+        lineTokenizer.setNames("id", "firstName", "lastName", "email", "gender", "contactNo", "country", "dob");
+
+        BeanWrapperFieldSetMapper<Customer> fieldSetMapper = new BeanWrapperFieldSetMapper<>();
+        fieldSetMapper.setTargetType(Customer.class);
+
+        lineMapper.setLineTokenizer(lineTokenizer);
+        lineMapper.setFieldSetMapper(fieldSetMapper);
+
+        return lineMapper;
+    }
+
+    // Item processor
+    @Bean
+    public CustomerProcessor customerProcessor() {
+        return new CustomerProcessor();
+    }
+
+    // Item writer
+    @Bean
+    public RepositoryItemWriter<Customer> customerWriter() {
+        RepositoryItemWriter<Customer> itemWriter = new RepositoryItemWriter<>();
+        itemWriter.setRepository(customerRepo);
+        itemWriter.setMethodName("save");
+        return itemWriter;
+    }
+
+    // Task executor
+    @Bean
+    public TaskExecutor taskExecutor() {
+        SimpleAsyncTaskExecutor taskExecutor = new SimpleAsyncTaskExecutor();
+        taskExecutor.setConcurrencyLimit(10);
+        return taskExecutor;
+    }
+
+    // Step creation without StepBuilderFactory
+    @Bean
+    public Step step() {
+        return new StepBuilder("step-1", jobRepository)
+                .<Customer, Customer>chunk(10, transactionManager)
+                .reader(customerReader())
+                .processor(customerProcessor())
+                .writer(customerWriter())
+                .taskExecutor(taskExecutor())
+                .build();
+    }
+
+    // Job creation without JobBuilderFactory
+    @Bean
+    public Job job() {
+        return new JobBuilder("job", jobRepository)
+                .start(step())
+                .build();
+    }
 }
